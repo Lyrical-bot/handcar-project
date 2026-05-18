b42f9c370481a990b1abdac7dc98eedc858cd5b5
Nyahong <iriszero1014@gmail.com>
2026-05-18 16:57:06 +0900

    feat: add Azure Maps geocoding API

---
+++ api/geocode/index.js
@@ -0,0 +1,43 @@
+﻿const { geocodeAddress } = require('../_shared/azureMapsClient');
+
+module.exports = async function (context, req) {
+  try {
+    const address = req.query.address || req.body?.address;
+
+    if (!address) {
+      context.res = {
+        status: 400,
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+        },
+        body: {
+          error: 'address query parameter is required.',
+          example: '/api/geocode?address=서울특별시 관악구 봉천로 412',
+        },
+      };
+      return;
+    }
+
+    const result = await geocodeAddress(address);
+
+    context.res = {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+      body: result,
+    };
+  } catch (error) {
+    context.log.error(error);
+
+    context.res = {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+      body: {
+        error: error.message,
+      },
+    };
+  }
+};
